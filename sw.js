2c37623c765d913541e3b0c09b958850f4088905
tgmmt2t6s8-pixel <tgmmt2t6s8@privaterelay.appleid.com>
2026-06-28 00:42:38 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
 self.addEventListener('install', (e) => {
   self.skipWaiting();
 });
-
 self.addEventListener('activate', (e) => {
   e.waitUntil(self.clients.claim());
 });
-
 self.addEventListener('fetch', (e) => {
-  // Obligatoriu lăsat deschis pentru a trece testul de instalare PWA
+  // Permite încărcarea offline a paginii de bază
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 });
