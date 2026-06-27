b4ce8a2f2c7ff94dd6b821878d2145d162d7e249
tgmmt2t6s8-pixel <tgmmt2t6s8@privaterelay.appleid.com>
2026-06-27 18:14:01 +0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Lasat gol intenționat pentru a permite instalarea PWA
+});
